bbf11baf2cf2f3a4a65df63acff6a8177409d4e7
chrism <cmoran@cmoran.xyz>
2025-04-30 12:30:00 -0400

    Remove cruft

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use cursive::Cursive;
 use cursive::event::Key;
 use cursive::views::{Button, Dialog, ResizedView, TextContent};
+use cursive::{
+    traits::*,
+    views::{Checkbox, EditView, LinearLayout, ListView, SelectView, TextArea, TextView},
+};
 
 struct Data {
     counter: i32,
@@ -17,13 +21,6 @@ fn main() {
     my_list(&mut siv);
     // list_view(&mut siv);
 
-    //siv.add_layer(Dialog::text("This is a bunch of text")
-    //    .title("My Title").button("Quit", |siv| show_next(siv)));
-
-    /*     siv.add_layer(Dialog::text("This is a survey!\nPress <Next> when you're ready.")
-    .title("Important survey")
-    .button("Next", show_next)); */
-
     siv.run();
 }
 
@@ -37,22 +34,6 @@ fn show_next(s: &mut Cursive) {
             .button("Uh?", |s| ()),
     ); //< And finally here too.
 }
-
-/* fn show_next(s: &mut Cursive) {
-    s.pop_layer();
-    s.add_layer(Dialog::text("Did you do the thing?")
-        .title("Question 1")
-        .button("Yes!", |s| show_answer(s, "I knew it! Well done!"))
-        .button("No!", |s| show_answer(s, "I knew you couldn't be trusted!"))
-        .button("Uh?", |s| s.add_layer(Dialog::info("Try again!"))));
-}
-
-fn show_answer(s: &mut Cursive, msg: &str) {
-    s.pop_layer();
-    s.add_layer(Dialog::text(msg)
-        .title("Results")
-        .button("Finish", |s| s.quit()));
-} */
 
 use cursive::view::SizeConstraint;
 fn my_list(siv: &mut Cursive) {
@@ -72,25 +53,6 @@ fn my_list(siv: &mut Cursive) {
 }
 
 fn counter(siv: &mut Cursive) {
-    // s.pop_layer();
-
-    /*     siv.add_layer(
-        Dialog::text("This uses some user data!")
-            .title("User data example")
-            .button("Increment", |s| {
-                // `Cursive::with_user_data()` is an easy way to run a closure
-                // on the data.
-                s.with_user_data(|data: &mut Data| {
-                    data.counter += 1;
-                });
-            })
-            .button("Show", |s| {
-                // `Cursive::user_data()` returns a reference to the data.
-                let value = s.user_data::<Data>().unwrap().counter;
-                s.add_layer(Dialog::info(format!("Current value: {value}")));
-            })
-            .button("Quit", Cursive::quit),
-    ); */
     let content = TextContent::new("content");
     let c2 = content.clone();
     let view = TextView::new_with_content(content.clone());
@@ -128,76 +90,5 @@ fn my_activity(s: &mut Cursive) {
             .button("Quit", |s| {
                 s.pop_layer();
             }),
-    );
-}
-
-use cursive::{
-    traits::*,
-    views::{Checkbox, EditView, LinearLayout, ListView, SelectView, TextArea, TextView},
-};
-
-fn list_view(siv: &mut Cursive) {
-    siv.add_layer(
-        Dialog::new()
-            .title("Please fill out this form")
-            .button("Ok", |s| s.quit())
-            .content(
-                ListView::new()
-                    // Each child is a single-line view with a label
-                    .child("Name", EditView::new().fixed_width(10))
-                    .child("Presentation", TextArea::new().min_height(4))
-                    .child(
-                        "Receive spam?",
-                        Checkbox::new().on_change(|s, checked| {
-                            // Enable/Disable the next field depending on this checkbox
-                            for name in &["email1", "email2"] {
-                                s.call_on_name(name, |view: &mut EditView| {
-                                    view.set_enabled(checked)
-                                });
-                                if checked {
-                                    s.focus_name("email1").unwrap();
-                                }
-                            }
-                        }),
-                    )
-                    .child(
-                        "Email",
-                        // Each child must have a height of 1 line,
-                        // but we can still combine multiple views!
-                        LinearLayout::horizontal()
-                            .child(
-                                EditView::new()
-                                    .disabled()
-                                    .with_name("email1")
-                                    .fixed_width(15),
-                            )
-                            .child(TextView::new("@"))
-                            .child(
-                                EditView::new()
-                                    .disabled()
-                                    .with_name("email2")
-                                    .fixed_width(10),
-                            ),
-                    )
-                    // Delimiter currently are just a blank line
-                    .delimiter()
-                    .child(
-                        "Age",
-                        // Popup-mode SelectView are small enough to fit here
-                        SelectView::new()
-                            .popup()
-                            .item_str("0-18")
-                            .item_str("19-30")
-                            .item_str("31-40")
-                            .item_str("41+"),
-                    )
-                    .with(|list| {
-                        // We can also add children procedurally
-                        for i in 0..50 {
-                            list.add_child(&format!("Item {i}"), EditView::new());
-                        }
-                    })
-                    .scrollable(),
-            ),
     );
 }
